dd312b8afaacca36ec9e735c6e7b219838b25c17
Alison Appling <aappling@usgs.gov>
2019-03-28 15:04:02 -0400

    work toward phase handling in apply_pgnn

--- 2_model/src/apply_pgnn.py
+++ 2_model/src/apply_pgnn.py
@@ -14,22 +14,22 @@ import tf_graph
 import tf_train
 
 def apply_pgnn(
+        phase = 'pretrain',
         learning_rate = 0.005,
-        state_size = 8,
-        ec_threshold = 24,
-        plam = 0.15,
-        elam = 0.00025,
-
+        state_size = 14, # Step 1: try number of input drivers, and half and twice that number
+        ec_threshold = 24, # TODO: calculate for each lake: take GLM temperatures, calculate error between lake energy and the fluxes, take the maximum as the threshold. Can tune from there, but it usually doesn’t change much from the maximum
+        plam = 0.15, # TODO: implement depth-density constraint in model
+        elam = 0.025, # original mille lacs values were 0.0005, 0.00025
         data_file = '1_format/tmp/pgdl_inputs/nhd_1099476.npz',
-
-        max_batch_obs = 100000,
-        n_epochs = 300,
+        max_batch_obs = 50000,
+        n_epochs = 5, # 200 is usually more than enough
         min_epochs_test = 0,
-        min_epochs_save = 250,
-        restore_path = '2_model/out/EC_mille/pretrain',
-        save_path = '2_model/out/EC_mille',
-        max_to_keep = 3,
+        min_epochs_save = 5, # later is recommended
+        restore_path = '', #'2_model/out/EC_mille/pretrain',
+        save_path = '2_model/tmp/nhd_1099476',
+        max_to_keep = 1,
         save_preds = True
+        # TODO: should have L1 and L2 norm weights in this list, implemented in tf_graph
     ):
     """Train (or pretrain) a PGRNN, optionally save the weights+biases and/or predictions, and return the predictions
 
@@ -39,9 +39,7 @@ def apply_pgnn(
         ec_threshold: Energy imbalance beyond which NN will be penalized
         plam: PRESENTLY IGNORED. physics (depth-density) constraint lambda, multiplier to physics loss when adding to other losses
         elam: energy constraint lambda, multiplier to energy balance loss when adding to other losses
-
         data_file: Filepath for the one file per lake that contains all the data.
-
         max_batch_obs: Upper limit on number of individual temperature predictions (date-depth-split combos) per batch. True batch size will be computed as the largest number of completes sequences for complete depth profiles that fit within this max_batch_size.
         n_epochs: Total number of epochs to run during training. Needs to be larger than the n epochs needed for the model to converge
         min_epochs_test: Minimum number of epochs to run through before computing test losses
@@ -71,15 +69,42 @@ def apply_pgnn(
 
     # %% Train model
     print('Training model...')
-    prd = tf_train.train_tf_graph(
-            train_op, cost, r_cost, pred, unsup_loss, x, y, m, unsup_inputs, unsup_phys_data,
-            inputs['train.features'], inputs['train.labels'], inputs['train.mask'],
-            inputs['unsup.features'], inputs['unsup.physics'],
-            inputs['test.features'], inputs['test.labels'], inputs['test.mask'],
-            seq_per_batch=seq_per_batch, n_epochs=n_epochs, min_epochs_test=min_epochs_test, min_epochs_save=min_epochs_save,
-            restore_path=restore_path, save_path=save_path, max_to_keep=max_to_keep, save_preds=save_preds)
+    if phase == 'tune':
+        train_stats, test_loss_RMSE, preds = tf_train.train_tf_graph(
+                train_op, cost, r_cost, pred, unsup_loss, x, y, m, unsup_inputs, unsup_phys_data,
+                inputs['tune_train.features'], inputs['tune_train.labels'], inputs['tune_train.mask'],
+                inputs['unsup.features'], inputs['unsup.physics'],
+                inputs['tune_test.features'], inputs['tune_test.labels'], inputs['tune_test.mask'], # test on training data
+                inputs['predict.features'],
+                seq_per_batch=seq_per_batch, n_epochs=n_epochs, min_epochs_test=min_epochs_test, min_epochs_save=min_epochs_save,
+                restore_path=restore_path, save_path=save_path, max_to_keep=max_to_keep, save_preds=save_preds)
+    elif phase == 'pretrain':
+        train_stats, test_loss_RMSE, preds = tf_train.train_tf_graph(
+                train_op, cost, r_cost, pred, unsup_loss, x, y, m, unsup_inputs, unsup_phys_data,
+                inputs['pretrain.features'], inputs['pretrain.labels'], inputs['pretrain.mask'],
+                inputs['unsup.features'], inputs['unsup.physics'],
+                inputs['pretrain.features'], inputs['pretrain.labels'], inputs['pretrain.mask'], # test on training data
+                inputs['predict.features'],
+                seq_per_batch=seq_per_batch, n_epochs=n_epochs, min_epochs_test=min_epochs_test, min_epochs_save=min_epochs_save,
+                restore_path=restore_path, save_path=save_path, max_to_keep=max_to_keep, save_preds=save_preds)
+    elif phase == 'train':
+        train_stats, test_loss_RMSE, preds = tf_train.train_tf_graph(
+                train_op, cost, r_cost, pred, unsup_loss, x, y, m, unsup_inputs, unsup_phys_data,
+                inputs['train.features'], inputs['train.labels'], inputs['train.mask'],
+                inputs['unsup.features'], inputs['unsup.physics'],
+                inputs['test.features'], inputs['test.labels'], inputs['test.mask'],
+                inputs['predict.features'],
+                seq_per_batch=seq_per_batch, n_epochs=n_epochs, min_epochs_test=min_epochs_test, min_epochs_save=min_epochs_save,
+                restore_path=restore_path, save_path=save_path, max_to_keep=max_to_keep, save_preds=save_preds)
+    else:
+        print("Error: unrecognized phase '%s'"% phase)
 
-    return(prd)
+    # %% Save model diagnostics
+    stats_save_file = '%s/preds.npy' % save_path
+    np.savez_compressed(stats_save_file, train_stats=train_stats, test_loss_RMSE=test_loss_RMSE)
+    print("  Diagnostics saved to %s" % stats_save_file)
+
+    return(preds)
     # %% Inspect predictions
 
     # prd has dimensions [depths*batches, n timesteps per batch, 1]
